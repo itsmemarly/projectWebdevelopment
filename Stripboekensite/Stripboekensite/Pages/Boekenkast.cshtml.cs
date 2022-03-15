64ce183c150bf679c03eacaf68f797ad62438b5a
Marleen ( 玛琳) <handyyou@live.nl>
2022-03-15 09:01:00 +0100

    Home pagina zit er in grote lijnen in, ben nu iets aan het proberen met login (staat nu op pagina Profiel)

---
+++ Stripboekensite/Stripboekensite/Pages/Boekenkast.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Stripboekensite.Pages
+{
+    public class BoekenkastModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
