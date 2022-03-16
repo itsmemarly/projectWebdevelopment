fc39288a25d736a4fd99532048645d8790ee1900
alissa de vegt <alissadevegt@gmail.com>
2022-03-16 16:29:44 +0100

    toevoeging van gebruiker aan database is klaar samen met delete van gebruiker en get op basis van ID(basis implementatie van crud)

--- Stripboekensite/Stripboekensite/Database/repositories/GebruikerRepository.cs
+++ Stripboekensite/Stripboekensite/Database/repositories/GebruikerRepository.cs
@@ -11,6 +11,16 @@ public class GebruikerRepository
         return new DbUtils().GetDbConnection();
     }
     
+    //geeft speciefiek gebruiker terug op basis id 
+    public Gebruiker Get(int gebruikerId)
+    {
+        string sql = "SELECT * FROM gebruikers WHERE Gebruikers_id = @gebruikerId";
+
+        using var connection = GetConnection();
+        var gebruiker = connection.QuerySingle<Gebruiker>(sql, new { gebruikerId });
+        return gebruiker;
+    }
+
     //geeft een IEnumerable lijst terug met gebruikers.
     public IEnumerable<Gebruiker> Get()
     {
@@ -20,4 +30,41 @@ public class GebruikerRepository
         var gebruikers  = connection.Query<Gebruiker>(sql);
         return gebruikers;
     }
+    //voegt nieuwe gebruiker toe
+    public Gebruiker Add(Gebruiker gebruiker)
+    {
+        string sql = @"
+                INSERT INTO gebruikers (Gebruikersnaam, versleuteld_wachtwoord,rol,naam) 
+                VALUES (@Gebruikersnaam,@versleuteld_wachtwoord, @rol, @naam); 
+                SELECT * FROM gebruikers WHERE Gebruikers_id = LAST_INSERT_ID()";
+
+        using var connection = GetConnection();
+        var nieuwgebruiker = connection.QuerySingle<Gebruiker>(sql, gebruiker);
+        return nieuwgebruiker;
+    }
+    
+    //verwijdert gebruiker
+    public bool Delete(int GebruikersId)
+    {
+        string sql = @"DELETE FROM gebruikers WHERE Gebruikers_id = @GebruikersId";
+
+        using var connection = GetConnection();
+        int numOfEffectedRows = connection.Execute(sql, new { GebruikersId });
+        return numOfEffectedRows == 1;
+    }
+    
+    public bool Update(Gebruiker gebruiker)
+    {
+        string sql = @"
+                UPDATE gebruikers SET 
+                    gebruikersnaam = @gebruikersnaam 
+                WHERE Gebruikers_id = @gebruikers_id;
+                SELECT * FROM gebruikers WHERE Gebruikers_id = @gebruikers_id";
+
+        using var connection = GetConnection();
+        var updatedgebruiker = connection.Execute(sql, gebruiker);
+        return updatedgebruiker==1;
+    }
+
+    
 }
