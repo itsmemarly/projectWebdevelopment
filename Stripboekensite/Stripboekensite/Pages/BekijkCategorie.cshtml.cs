f0b4d5555ff90a1112b37924fdf5fd9793b25bae
Marleen ( 玛琳) <handyyou@live.nl>
2022-03-23 12:24:26 +0100

    Pagina's toegevoegd voor moderators via dashboard te bereiken

---
+++ Stripboekensite/Stripboekensite/Pages/BekijkCategorie.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Stripboekensite.Pages;
+
+public class BekijkCategorie : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
---
+++ Stripboekensite/Stripboekensite/Pages/BekijkGebruikers.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Stripboekensite.Pages;
+
+public class BekijkGebruikers : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
